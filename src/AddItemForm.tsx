5dfa77c6c85449393fee026039af858858443039
Julia <yulia.bobko@mail.ru>
2023-04-25 23:36:22 +0300

    created a component AddItemForm

---
+++ src/AddItemForm.tsx
@@ -0,0 +1,45 @@
+import React, {ChangeEvent, useState} from "react";
+
+type AddItemFormProps = {
+    addItem: (title: string) => void
+}
+
+export function AddItemForm(props: AddItemFormProps) {
+    const [error, setError] = useState<string>("");
+    const [newTitle, setNewTitle] = useState<string>('')
+
+    function onChangeTitle(e: ChangeEvent<HTMLInputElement>) {
+        setError('')
+        if (e.currentTarget.value.trim() !== '') {
+            setNewTitle(e.currentTarget.value.trim())
+        }
+    }
+
+    function onClickAddTaskHandler() {
+        addTask()
+    }
+
+    function addTask() {
+        if (newTitle.trim() !== '') {
+            props.addItem(newTitle)
+            setNewTitle('')
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    function onKeyDownHandler(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === 'Enter') {
+            addTask()
+        }
+    }
+
+    return (
+        <div>
+            <input className={error && 'error_input'} value={newTitle} onChange={onChangeTitle}
+                   onKeyDown={onKeyDownHandler}/>
+            <button onClick={onClickAddTaskHandler}>+</button>
+            {error && <div className={'error_message'}>{error}</div>}
+        </div>
+    )
+}
